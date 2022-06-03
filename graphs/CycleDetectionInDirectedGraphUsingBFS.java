827f8321d46e9d519b620b6f458a53005ec182a6
sandeep <sandeepprabhakula10@gmail.com>
2022-06-03 23:20:27 +0530

    3 jun 2022

---
+++ graphs/CycleDetectionInDirectedGraphUsingBFS.java
@@ -0,0 +1,48 @@
+package com.sandeepprabhakula.graphs;
+
+import java.util.*;
+
+public class CycleDetectionInDirectedGraphUsingBFS {
+    // Topological sort : Linear ordering of the nodes which having an edge u --> v where u appears first in the list then v;
+    // TopologicalSort is only possible for "Directed Acyclic Graph"(DAG);
+    // It is also called Kahn's Algorithm
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        List<List<Integer>>adj = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<m;i++){
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            adj.get(u).add(v);
+        }
+        System.out.println(topoSort(adj,n));
+    }
+    public static boolean topoSort(List<List<Integer>>adj,int n){
+        // inDegree is number of nodes coming in to the node;
+        int[]inDegree = new int[n];
+        for(int i=0;i<n;i++){
+            for(int it:adj.get(i)){
+                inDegree[it]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(inDegree[i]==0)q.add(i);
+        }
+        int count = 0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            count++;
+            for(int k:adj.get(node)){
+                inDegree[k]--;
+                if(inDegree[k]==0)q.add(k);
+            }
+        }
+        return count != n;
+    }
+}
